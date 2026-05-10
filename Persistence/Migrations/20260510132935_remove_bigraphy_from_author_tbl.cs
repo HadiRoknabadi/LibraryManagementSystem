160a3fcf7095a8126111b3fab10c312e4dcd878c
Hadi Roknabadi <dev.hadiroknabadi@gmail.com>
2026-05-10 17:00:03 +0330

    feat: remove Biography field from Author entity

---
+++ Persistence/Migrations/20260510132935_remove_bigraphy_from_author_tbl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class remove_bigraphy_from_author_tbl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Biography",
+                table: "Authors");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Biography",
+                table: "Authors",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
