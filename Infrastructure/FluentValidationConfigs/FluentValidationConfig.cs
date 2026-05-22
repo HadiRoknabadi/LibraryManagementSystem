3446fcecac57dead09c78e3671c17c3ab63f2f1f
Hadi Roknabadi <dev.hadiroknabadi@gmail.com>
2026-05-22 16:10:00 +0330

    feat: implement auto-registration for fluent validators

--- Infrastructure/FluentValidationConfigs/FluentValidationConfig.cs
+++ Infrastructure/FluentValidationConfigs/FluentValidationConfig.cs
@@ -1,12 +1,4 @@
 ﻿using Application.DTOs.Account;
-using Application.DTOs.Author;
-using Application.DTOs.Book;
-using Application.DTOs.BookCategory;
-using Application.DTOs.BookCopy;
-using Application.DTOs.Borrowing;
-using Application.DTOs.Publisher;
-using Application.DTOs.User;
-using Domain.Entities.Book;
 using FluentValidation;
 using FluentValidation.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -19,67 +11,7 @@ namespace Infrastructure.FluentValidationConfigs
         {
             services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
 
-            #region User
-
-            services.AddScoped<IValidator<AddUserDTO>, AddUserDTOValidator>();
-            services.AddScoped<IValidator<EditUserDTO>, EditUserDTOValidator>();
-
-            #endregion
-
-            #region Account
-
-            services.AddScoped<IValidator<LoginUserDTO>, LoginUserDTOValidator>();
-
-
-            #endregion
-
-            #region Book Category
-
-            services.AddScoped<IValidator<AddBookCategoryDTO>, AddBookCategoryDTOValidator>();
-            services.AddScoped<IValidator<EditBookCategoryDTO>, EditBookCategoryDTOValidator>();
-
-
-            #endregion
-
-            #region Author
-
-            services.AddScoped<IValidator<AddAuthorDTO>, AddAuthorDTOValidator>();
-            services.AddScoped<IValidator<EditAuthorDTO>, EditAuthorDTOValidator>();
-
-
-            #endregion
-
-            #region Publisher
-
-            services.AddScoped<IValidator<AddPublisherDTO>, AddPublisherDTOValidator>();
-            services.AddScoped<IValidator<EditPublisherDTO>, EditPublisherDTOValidator>();
-
-
-            #endregion
-
-            #region Book
-
-            services.AddScoped<IValidator<AddBookDTO>, AddBookDTOValidator>();
-            services.AddScoped<IValidator<EditBookDTO>, EditBookDTOValidator>();
-
-
-            #endregion
-
-            #region Book Copy
-
-            services.AddScoped<IValidator<AddBookCopyDTO>, AddBookCopyDTOValidator>();
-            services.AddScoped<IValidator<EditBookCopyDTO>, EditBookCopyDTOValidator>();
-
-            #endregion
-
-            #region Borrow 
-
-            services.AddScoped<IValidator<SubmitBorrowDTO>, SubmitBorrowDTOValidator>();
-            services.AddScoped<IValidator<EditBorrowDTO>, EditBorrowDTOValidator>();
-
-
-            #endregion
-
+            services.AddValidatorsFromAssemblyContaining<LoginUserDTOValidator>();
 
             return services;
         }
