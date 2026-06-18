223644e72059bd105635f21e3880380174bcd213
Hadi Roknabadi <dev.hadiroknabadi@gmail.com>
2026-06-18 17:22:02 +0330

    add: add and edit book category dto validator  tests

---
+++ Application.UnitTests/Validators/BookCategory/AddBookCategoryDTOValidatorTests.cs
@@ -0,0 +1,62 @@
+﻿using Application.DTOs.BookCategory;
+using AutoFixture.Xunit2;
+using FluentValidation.TestHelper;
+
+public class AddBookCategoryDTOValidatorTests
+{
+    private readonly AddBookCategoryDTOValidator _validator = new();
+
+    [Fact]
+    public void Title_Should_Have_Error_When_Empty()
+    {
+        // Arrange
+        var model = new AddBookCategoryDTO
+        {
+            Title = string.Empty
+        };
+
+        // Act
+        var result = _validator.TestValidate(model);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Title);
+    }
+
+    [Theory]
+    [AutoData]
+    public void Title_Should_Have_Error_When_Length_Greater_Than_150(string random)
+    {
+        // Arrange
+        var title = random.PadRight(151, 'a');
+
+        var model = new AddBookCategoryDTO
+        {
+            Title = title
+        };
+
+        // Act
+        var result = _validator.TestValidate(model);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Title);
+    }
+
+    [Theory]
+    [AutoData]
+    public void Title_Should_Not_Have_Error_When_Valid(string random)
+    {
+        // Arrange
+        var title = random.PadRight(50, 'a').Substring(0, 50);
+
+        var model = new AddBookCategoryDTO
+        {
+            Title = title
+        };
+
+        // Act
+        var result = _validator.TestValidate(model);
+
+        // Assert
+        result.ShouldNotHaveValidationErrorFor(x => x.Title);
+    }
+}
