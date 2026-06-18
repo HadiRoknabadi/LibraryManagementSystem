97e9612f6a9a17e9c13ecf26672578eb911b13ce
Hadi Roknabadi <dev.hadiroknabadi@gmail.com>
2026-06-18 17:29:49 +0330

    add: add and edit book copy  dto validator tests

---
+++ Application.UnitTests/Validators/BookCopy/AddBookCopyDTOValidatorTests.cs
@@ -0,0 +1,46 @@
+﻿using Application.DTOs.BookCopy;
+using AutoFixture.Xunit2;
+using FluentValidation.TestHelper;
+
+public class AddBookCopyDTOValidatorTests
+{
+    private readonly AddBookCopyDTOValidator _validator = new();
+
+    [Theory]
+    [AutoData]
+    public void ShelfLocation_Should_Have_Error_When_Length_Greater_Than_100(string random)
+    {
+        // Arrange
+        var shelfLocation = random.PadRight(101, 'a');
+
+        var model = new AddBookCopyDTO
+        {
+            ShelfLocation = shelfLocation
+        };
+
+        // Act
+        var result = _validator.TestValidate(model);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.ShelfLocation);
+    }
+
+    [Theory]
+    [AutoData]
+    public void ShelfLocation_Should_Not_Have_Error_When_Valid(string random)
+    {
+        // Arrange
+        var shelfLocation = random.PadRight(50, 'a').Substring(0, 50);
+
+        var model = new AddBookCopyDTO
+        {
+            ShelfLocation = shelfLocation
+        };
+
+        // Act
+        var result = _validator.TestValidate(model);
+
+        // Assert
+        result.ShouldNotHaveValidationErrorFor(x => x.ShelfLocation);
+    }
+}
