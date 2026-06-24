d624bbd2d14389915a661cd94a1e4782369c3c89
Hadi Roknabadi <dev.hadiroknabadi@gmail.com>
2026-06-24 16:30:57 +0330

    add: initial E2E test project setup

---
+++ WebSite.EndPoint.E2ETests/Infrastructure/CustomWebApplicationFactory.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Context;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebSite.EndPoint.E2ETests.Infrastructure
+{
+    public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+
+                if (descriptor != null)
+                {
+                    services.Remove(descriptor);
+                }
+
+
+                services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("E2E_Test_Database");
+                });
+
+                var sp = services.BuildServiceProvider();
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<ApplicationDbContext>();
+                    db.Database.EnsureCreated();
+                }
+            });
+        }
+    }
+}
